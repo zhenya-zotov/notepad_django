d7ca04f18e7e56057d0e91bb0e2bdeebb57428ec
Zhenya Zotov <78563705+zhenya-zotov@users.noreply.github.com>
2024-01-30 00:36:39 +0200

    Step 3

--- nodepad/notes/views.py
+++ nodepad/notes/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Note
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Nodes")
+    latest_note_list = Note.objects.all()
+    context = {
+        "latest_note_list": latest_note_list,
+    }
+    return render(request, "notes/index.html", context)
+
+def detail(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, "notes/detail.html", {"note": note})
